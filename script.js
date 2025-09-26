2304091dd2c3b95c69ce551783f46d2b22c823f6
habul17 <abulhassanca.dev@gmail.com>
2025-09-26 10:24:38 +0530

    Improved readability of the code

--- script.js
+++ script.js
@@ -10,6 +10,23 @@ const jsxHeading = (
     </h1>
 )
 
+// Let us use functional components - just a normal function
+
+const HeadingComponent = () => {
+    return <h1>
+        "From Functional components - explicit return"
+    </h1>
+}
+
+// implicit return
+
+const HeadingComponent1 = () => (
+    <div id='container'>
+        <h1>"From Functional component - implicit return"</h1>
+    </div>
+)
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent1 />);
